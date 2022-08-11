c886b58e5ebd083054360bc9693b083b54d787ad
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-10 22:35:43 -0700

    populated readme, barebones and docstrings up in caesar_cipher.py -- revisiting tomorrow

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,25 @@
-
+### Need access to Corpus of some kind
 
 
 # chars = [0...9]
 
 
+from asyncio.proactor_events import _ProactorBaseWritePipeTransport
+
+
 def encrypt(plain, shift):
+  """
+  Function that takes in a plain text phrase and a numeric shift.
+
+  - The phrase will then be shifted that many letters.
+    - E.g. encrypt(‘abc’,1) would return ‘bcd’. = E.g. - encrypt  (‘abc’, 10) would return ‘klm’.
+
+  - Shifts that exceed 26 should wrap around.
+    - E.g. encrypt(‘abc’,27) would return ‘bcd’.
+
+  - Shifts that push a letter out of range should wrap around.
+    - E.g. encrypt('zzz', 1) would return 'aaa'.
+  """
   encrypted = ""
   for char in plain:
     num = int(char)
@@ -23,9 +38,20 @@ def encrypt(plain, shift):
   # return plain + " coming soon"
 
 def decrypt(cipher, shift):
-  """"""
+  """
+  Function that takes in encrypted text and numeric shift which will restore the encrypted text back to its original form when correct key is supplied.
+  """
   # return cipher + " coming soon"
   return encrypt(cipher, -shift)
+
+
+def crack():
+  """
+  Function that will decode the cipher so that an encrypted message can be transformed into its original state WITHOUT access to the key.
+
+  Devise a method for the computer to determine if code was broken with minimal human guidance.
+  """
+
 
 if __name__ == "__main__":
   pins = []
